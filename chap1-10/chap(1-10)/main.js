248bc2d0a7b863f49a52dce4536216ab99a1b011
uroojshakeel <165478020+uroojshakeel@users.noreply.github.com>
2024-09-17 23:10:53 +0500

    Add files via upload

---
+++ chap1-10/chap(1-10)/main.js
@@ -0,0 +1,41 @@
+// --------Q1----------
+
+alert("urooj shakeel");
+
+
+// --------Q2----------
+
+var myName = ("urooj shakeel")
+alert("hello, my name is" +" "+ myName);
+console.log(myName);
+
+
+// --------Q3----------
+
+var a = "4";
+var b = "2";
+var result = (a++ + --b  - --a + b++ + a + b);
+console.log(result);
+
+
+// --------Q4---------- 
+
+var number = prompt("How many points did you receive in the exam?")
+if (number>=90){
+    alert("A++");
+}
+else if (number>=80){
+    alert("A+");
+}
+else if (number>=70){
+    alert("A");
+}
+else if (number>=60){
+    alert("C");
+}
+else if (number>=50){
+    alert("D");
+}
+else if (number>=49){
+    alert("Fail");
+}
